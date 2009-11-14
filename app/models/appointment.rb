192d19aaaec3a947e46ebfbefcb320d6d2d09375
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2009-11-14 10:42:54 -0500

    Add validations to Appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,5 @@
 class Appointment < ActiveRecord::Base
   
+  validates_presence_of :last_name, :first_name, :phone, :alternate_phone, :cat_or_dog, :gender, :age, :vaccinated, :pregnant
+  
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,13 +1,59 @@
 require 'spec_helper'
 
-describe Appointment do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
+describe Appointment, "being valid" do
+  
+  it "requires last name" do
+    appt = Appointment.new(:last_name => nil)
+    appt.should_not be_valid
+    appt.should have(1).error_on(:last_name)
   end
-
-  it "should create a new instance given valid attributes" do
-    Appointment.create!(@valid_attributes)
+  
+  it "requires first name" do
+    appt = Appointment.new(:first_name => nil)
+    appt.should_not be_valid
+    appt.should have(1).error_on(:first_name)
   end
+  
+  it "requires phone number" do
+    appt = Appointment.new(:phone => nil)
+    appt.should_not be_valid
+    appt.should have(1).error_on(:phone)
+  end
+  
+  it "requires alternate phone number" do
+    appt = Appointment.new(:alternate_phone => nil)
+    appt.should_not be_valid
+    appt.should have(1).error_on(:alternate_phone)
+  end
+  
+  it "requires cat or dog" do
+    appt = Appointment.new(:cat_or_dog => nil)
+    appt.should_not be_valid
+    appt.should have(1).error_on(:cat_or_dog)
+  end
+  
+  it "requires gender" do
+    appt = Appointment.new(:gender => nil)
+    appt.should_not be_valid
+    appt.should have(1).error_on(:gender)
+  end
+  
+  it "requires age" do
+    appt = Appointment.new(:age => nil)
+    appt.should_not be_valid
+    appt.should have(1).error_on(:age)
+  end
+  
+  it "requires vaccinated" do
+    appt = Appointment.new(:vaccinated => nil)
+    appt.should_not be_valid
+    appt.should have(1).error_on(:vaccinated)
+  end
+  
+  it "requires pregnant" do
+    appt = Appointment.new(:pregnant => nil)
+    appt.should_not be_valid
+    appt.should have(1).error_on(:pregnant)
+  end
+  
 end
