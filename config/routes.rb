e12a1cae18cb5980d4433e7dbf202065b5c46137
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2009-11-14 10:39:40 -0500

    user model w/ session conroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 ActionController::Routing::Routes.draw do |map|
+
+  map.root :controller => :appointments
+  map.resources :appointments
+
+  map.logout   '/logout',   :controller => 'sessions', :action => 'destroy'
+  map.login    '/login',    :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users',    :action => 'create'
+  map.signup   '/signup',   :controller => 'users',    :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -17,7 +29,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Sample resource route with sub-resources:
   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
+
   # Sample resource route with more complex sub-resources
   #   map.resources :products do |products|
   #     products.resources :comments
@@ -38,10 +50,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.resources :appointments
-  
-  map.root :controller => :appointments
-  
+
   # map.connect ':controller/:action/:id'
   # map.connect ':controller/:action/:id.:format'
 end
