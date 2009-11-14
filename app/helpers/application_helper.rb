821eaeddf9bafde0eec08d780c9e123bbb2d1961
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2009-11-14 12:01:17 -0500

    basic login and appointments listing for staff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def flash_div(*keys)
+    return '' if keys.blank?
+
+    flash_info = keys.collect { |key|
+      content_tag(:div,
+                  h(flash[key]), :id => "flash_#{key}", :class => "#{key} flash") if flash[key]
+    }.join
+
+    flash_info
+  end
+
 end
