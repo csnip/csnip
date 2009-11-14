abe476b2c73ffa5c7de93bf10ffade4be63b11f1
Dustin Tinney <tinney@atomicobject.com>
2009-11-14 16:37:23 -0500

    updated new fomr with info from pat's request

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,5 @@
 class Appointment < ActiveRecord::Base
   
-  validates_presence_of :last_name, :first_name, :phone, :alternate_phone, :cat_or_dog, :gender, :age, :vaccinated, :pregnant
+  validates_presence_of :last_name, :first_name, :phone, :pet_name, :cat_or_dog, :gender, :age, :vaccinated, :pregnant
   
 end
