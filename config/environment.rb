b15d568bc7ed217066e4c93b44a9ef80f86f88db
Dustin Tinney <tinney@atomicobject.com>
2009-11-15 09:23:44 -0500

    updating appointments to show errors correctly

--- config/environment.rb
+++ config/environment.rb
@@ -40,3 +40,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+ActionView::Base.field_error_proc = Proc.new {|html_tag, instance|  %(<span class="form-errors">#{html_tag}</span>)}
+
