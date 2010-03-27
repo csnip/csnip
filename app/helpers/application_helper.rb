bcf2c89e00b2ee3c7a154a1a746b6ccec1991018
Kevin Marsh <kevin.marsh@gmail.com>
2010-03-27 18:01:00 -0400

    formatted_boolean helper method for formatting boolean/NULL values as Yes/No/Not Sure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,20 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def formatted_boolean(value, bold_false = false, nil_label = "Not Sure")
+    case value
+      when nil
+        nil_label
+      when true
+        "Yes"
+      when false
+        if bold_false
+          content_tag(:strong, "No")
+        else
+          "No"
+        end
+    end
+  end
+  
   def flash_div(*keys)
     return '' if keys.blank?
 
